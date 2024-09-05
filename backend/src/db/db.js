23a491222d3224be99312db2d7da7291e87fefe1
LEONARDO VINICIUS SILVA BATISTA <leovinicius3737@gmail.com>
2024-09-05 13:46:17 -0400

    add dockerfile in be and db

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -6,6 +6,7 @@ const db = new Sequelize({
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 })
 
 export default db;
